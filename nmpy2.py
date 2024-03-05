8d8e39aba6c2f9909a890fa89e1e18b3d9ed39ae
YAZILIMCIABII <atunc8402@gmail.com>
2024-03-05 18:38:22 +0300

    We added 2 file that wrote.

---
+++ nmpy2.py
@@ -0,0 +1,75 @@
+import numpy as np
+
+import time
+
+# start_time = time.time()
+
+# A = np.arange(1000000)
+
+# A ** 2
+
+# Time_1 = time.time() - start_time
+
+# start_time_2 = time.time()
+
+# L = range(1000000)
+
+# [i ** 2 for i in L]
+
+# Time_2 = time.time() - start_time_2
+
+# print(Time_2 / Time_1)
+
+
+# x = np.array([1,2,3,4])
+
+# print(type(x)) # x type
+
+# print(x.size) # size of x
+
+# print(x.shape) # shape of x
+
+# print(x.ndim) # dimension of x
+
+# print(x.nbytes) # byte of x
+
+# print(x.dtype) # data type of x
+
+
+# y = np.array([[1,2,3],[4,5,6]])
+
+# print(type(y)) # x type
+
+# print(y) 
+
+# print(y.size) # size of x
+
+# print(y.shape) # shape of x
+
+# print(y.ndim) # dimension of x
+
+# print(y.nbytes) # byte of x
+
+z = np.array([[[1,2],[3,4]],[[1,2],[3,4]]])
+
+print(z.ndim)
+
+print(z.shape)
+
+a = np.array([1,2,3])
+
+b = np.array([1,"2",3])
+
+c = np.array([1,2.0,3])
+
+print(a.dtype)
+
+print(b.dtype)
+
+print(c.dtype)
+
+
+
+
+
+# print(y.dtype) # data type of x
